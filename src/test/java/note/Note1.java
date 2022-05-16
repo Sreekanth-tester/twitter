5b5e866bb7c75c4fdf4394d98b9a976cfac6a853
Sreekanth <srikanthdevella97@gmail.com>
2022-05-16 10:55:10 +0530

    pushing project

---
+++ src/test/java/note/Note1.java
@@ -0,0 +1,16 @@
+package note;
+
+public class Note1 {
+
+	public static void main(String[] args) 
+	{
+		String x="kalamsir";
+		int len=x.length();
+		char c=x.charAt(len-1);
+		System.out.println(c);
+		
+		
+
+	}
+
+}
